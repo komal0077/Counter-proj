5881cb32eb61e8cea695d933803ca46d0de67268
komal <komalme7@gmail.com>
2024-08-28 16:21:57 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
+
 import './App.css';
+import react from "react"
+import { useState } from 'react';
+
 
 function App() {
+  const [count,setCount]=useState(0);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="h-screen flex items-center justify-center">
+    <div className="card text-center ">
+<div className='card-body border-2 border-black p-10 inline-block '>
+<h1>Counter App</h1>
+<h2>{count}</h2>
+<div className='my-5 flex justify-center'>
+  
+  <button className='btn mx-3 bg-cyan-600' onClick={()=>setCount(count+1)}>Increment</button>
+  <button className='btn mx-3  bg-red-500' onClick={()=>setCount(count-1)}>Decrement</button>
+  <button className='btn mx-3 bg-green-400' onClick={()=>setCount(0)}>Reset</button>
+</div>
+</div>
+
+    </div>
+    
     </div>
   );
 }
